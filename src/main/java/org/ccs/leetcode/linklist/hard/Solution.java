bd164337f17c995f4b3b32b11cee3622b04b2349
liqiang.fang <liqiang.fang@qunar.com>
2017-06-29 18:03:01 -0700

    copyRight

---
+++ src/main/java/org/ccs/leetcode/linklist/hard/Solution.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Liqiang Fang. All Rights Reserved.
+ */
+package org.ccs.leetcode.linklist.hard;
+
+/**
+ * @author Abel created on 2017/6/29 18:01
+ * @version $Id$
+ */
+public class Solution {
+}
---
+++ src/main/java/org/ccs/leetcode/linklist/medium/Solution.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Liqiang Fang All Rights Reserved.
+ */
+package org.ccs.leetcode.linklist.medium;
+
+/**
+ * @author Abel created on 2017/6/29 18:02
+ * @version $Id$
+ */
+public class Solution {
+}
