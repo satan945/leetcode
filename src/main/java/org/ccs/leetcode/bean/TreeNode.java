493551c57625cff6a0ca928ce7552d98a3cb0957
liqiang.fang <liqiang.fang@qunar.com>
2017-06-29 17:59:18 -0700

    treeNode

---
+++ src/main/java/org/ccs/leetcode/bean/TreeNode.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Qunar.com. All Rights Reserved.
+ */
+package org.ccs.leetcode.bean;
+
+/**
+ * @author Abel created on 2017/6/29 17:58
+ * @version $Id$
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
