c212dad35a15907f178074a524c7980ff8d0d2fa
liqiang.fang <liqiang.fang@qunar.com>
2017-07-07 11:37:06 -0700

    add Two Numbers by BigDecimal

---
+++ src/main/java/org/ccs/leetcode/bean/ListNode.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Qunar.com. All Rights Reserved.
+ */
+package org.ccs.leetcode.bean;
+
+/**
+ * @author Abel created on 2017/6/29 23:00
+ * @version $Id$
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
--- src/main/java/org/ccs/leetcode/linklist/medium/Solution.java
+++ src/main/java/org/ccs/leetcode/linklist/medium/Solution.java
@@ -3,9 +3,60 @@
  */
 package org.ccs.leetcode.linklist.medium;
 
+import org.ccs.leetcode.bean.ListNode;
+
+import java.math.BigDecimal;
+import java.util.Stack;
+
 /**
  * @author Abel created on 2017/6/29 18:02
  * @version $Id$
  */
 public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        BigDecimal num1 = new BigDecimal(0);
+        BigDecimal num2 = new BigDecimal(0);
+        BigDecimal multi = new BigDecimal(1);
+        ListNode l1Cursor = l1;
+        while (l1Cursor != null) {
+            num1 = num1.add(new BigDecimal(l1Cursor.val).multiply(multi));
+            multi = multi.multiply(BigDecimal.TEN);
+            l1Cursor = l1Cursor.next;
+        }
+        multi = new BigDecimal(1);
+        ListNode l2Cursor = l2;
+        while (l2Cursor != null) {
+            num2 = num2.add(new BigDecimal(l2Cursor.val).multiply(multi));
+            multi = multi.multiply(BigDecimal.TEN);
+            l2Cursor = l2Cursor.next;
+        }
+        BigDecimal sum = num1.add(num2);
+        if(sum.equals(BigDecimal.ZERO)){
+            return new ListNode(0);
+        }
+        String sumStr = new StringBuilder(sum.toString()).reverse().toString();
+        ListNode fakeHead = new ListNode(0);
+        ListNode curNode = fakeHead;
+        for (int i = 0; i < sumStr.length(); i++) {
+            ListNode node = new ListNode(Integer.parseInt(sumStr.substring(i, i + 1)));
+            curNode.next = node;
+            curNode = node;
+        }
+        return fakeHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l11 = new ListNode(4);
+        ListNode l12 = new ListNode(3);
+        l1.next=l11;
+        l11.next=l12;
+        ListNode l2 = new ListNode(5);
+        ListNode l21 = new ListNode(6);
+        ListNode l22 = new ListNode(4);
+        l2.next=l21;
+        l21.next=l22;
+
+        new Solution().addTwoNumbers(l1, l2);
+    }
 }
