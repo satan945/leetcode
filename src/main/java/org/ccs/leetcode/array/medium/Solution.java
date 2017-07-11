5d9101d809d8a525f1290a6405d26d5bfd2b814b
liqiang.fang <liqiang.fang@qunar.com>
2017-07-11 10:40:38 -0700

    2Sum

--- src/main/java/org/ccs/leetcode/array/medium/Solution.java
+++ src/main/java/org/ccs/leetcode/array/medium/Solution.java
@@ -3,6 +3,10 @@
  */
 package org.ccs.leetcode.array.medium;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Abel created on 2017/7/10 17:47
  * @version $Id$
@@ -43,18 +47,9 @@ public class Solution {
 
     /**
      * <p>
-     * The idea was firstly transpose the matrix and then flip it symmetrically. For instance,
-     * 1 2 3
-     * 4 5 6
-     * 7 8 9
-     * after transpose, it will be swap(matrix[i][j], matrix[j][i])
-     * 1 4 7
-     * 2 5 8
-     * 3 6 9
-      * Then flip the matrix horizontally. (swap(matrix[i][j], matrix[i][matrix.length-1-j])
-     * 7 4 1
-     * 8 5 2
-     * 9 6 3
+     * The idea was firstly transpose the matrix and then flip it symmetrically. For instance, 1 2 3 4 5 6 7 8 9 after
+     * transpose, it will be swap(matrix[i][j], matrix[j][i]) 1 4 7 2 5 8 3 6 9 Then flip the matrix horizontally.
+     * (swap(matrix[i][j], matrix[i][matrix.length-1-j]) 7 4 1 8 5 2 9 6 3
      * </p>
      * 
      * @param matrix
@@ -76,6 +71,58 @@ public class Solution {
                 matrix[i][matrix.length - 1 - j] = temp;
             }
         }
+    }
+
+    /**
+     * 15. 3Sum
+     * <p>
+     * https://leetcode.com/problems/3sum
+     * <p>
+     * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets
+     * in the array which gives the sum of zero.
+     * 
+     * Note: The solution set must not contain duplicate triplets.
+     * 
+     * For example, given array S = [-1, 0, 1, 2, -1, -4],
+     * 
+     * A solution set is: [ [-1, 0, 1], [-1, -1, 2] ]
+     * </p>
+     * 
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            int target = -nums[i];
+            while (k > j) {
+                if (nums[j] + nums[k] == target) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                } else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
