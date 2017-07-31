d6ef800fb5167a0d4ac4b825616f7568298a32f2
liqiang.fang <liqiang.fang@qunar.com>
2017-07-31 14:37:19 -0700

    Tree2Str TreeLevelAccess

--- src/main/java/org/ccs/leetcode/array/easy/Solution.java
+++ src/main/java/org/ccs/leetcode/array/easy/Solution.java
@@ -4,6 +4,7 @@
 package org.ccs.leetcode.array.easy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -91,6 +92,7 @@ public class Solution {
      * The input array may contain duplicates, so ascending order here means <=.
      * </p>
      * todo
+     * 
      * @param nums
      * @return
      */
@@ -98,10 +100,44 @@ public class Solution {
         return 1;
     }
 
+    /**
+     * 561. Array Partition I
+     *
+     * <p>
+     * https://leetcode.com/problems/array-partition-i/
+     * <p>
+     * Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2,
+     * b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+     * 
+     * Example 1: Input: [1,4,3,2]
+     * 
+     * Output: 4
+     * 
+     * Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+     * 
+     * Note: n is a positive integer, which is in the range of [1, 10000]. All the integers in the array will be in the
+     * range of [-10000, 10000].
+     * </p>
+     * 
+     * @param nums
+     * @return
+     */
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums1 = new int[] { 1 };
-        int[] nums2 = new int[] { 1, 2 };
-        System.out.println(solution.intersection(nums1, nums2));
+        int[] nums2 = new int[] { -1, -2, 1, 4, 2, 3 };
+        // System.out.println(solution.intersection(nums1, nums2));
+        System.out.println(solution.arrayPairSum(nums2));
     }
 }
--- src/main/java/org/ccs/leetcode/binarytree/easy/Solution.java
+++ src/main/java/org/ccs/leetcode/binarytree/easy/Solution.java
@@ -3,6 +3,11 @@
  */
 package org.ccs.leetcode.binarytree.easy;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 import org.ccs.leetcode.bean.TreeNode;
 
 /**
@@ -12,17 +17,56 @@ import org.ccs.leetcode.bean.TreeNode;
 public class Solution {
 
     /**
-     * todo
+     * 606. Construct String from Binary Tree
+     * <p>
+     * https://leetcode.com/problems/construct-string-from-binary-tree
+     * <p>
+     * You need to construct a string consists of parenthesis and integers from a binary tree with the preorder
+     * traversing way.
+     * 
+     * The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty
+     * parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original
+     * binary tree.
+     * 
+     * Example 1: Input: Binary tree: [1,2,3,4] 1 / \ 2 3 / 4
+     * 
+     * Output: "1(2(4))(3)"
+     * 
+     * Explanation: Originallay it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty
+     * parenthesis pairs. And it will be "1(2(4))(3)". Example 2: Input: Binary tree: [1,2,3,null,4] 1 / \ 2 3 \ 4
+     * 
+     * Output: "1(2()(4))(3)"
+     * 
+     * Explanation: Almost the same as the first example, except we can't omit the first parenthesis pair to break the
+     * one-to-one mapping relationship between the input and the output.
+     * </p>
+     * 
      * @param t
      * @return
      */
     public String tree2str(TreeNode t) {
         if (t == null) {
-            return "()";
+            return "";
         }
-        String leftStr = tree2str(t.left);
-        String rightStr = tree2str(t.right);
-        return t.val + "(" + leftStr + rightStr + ")";
+        String leftStr;
+        String rightStr;
+        if (t.left == null && t.right == null) {
+            return String.valueOf(t.val);
+        }
+        leftStr = tree2str(t.left);
+        rightStr = tree2str(t.right);
+        return t.val + (t.left == null ? "()" : "(" + leftStr + ")") + (t.right == null ? "" : "(" + rightStr + ")");
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode n1 = new TreeNode(2);
+        TreeNode n2 = new TreeNode(3);
+        TreeNode n3 = new TreeNode(4);
+        root.left = n1;
+        root.right = n2;
+        n1.right = n3;
+        System.out.println(new Solution().tree2str(root));
     }
 
     /**
@@ -77,14 +121,12 @@ public class Solution {
         }
     }
 
-
-
     /**
-     * 100. Same Tree QuestionEditorial Solution My Submissions Total Accepted: 144793 Total Submissions: 329034
-     * Difficulty: Easy Given two binary trees, write a function to check if they are equal or not.
-     *
+     * 100. Same Tree QuestionEditorial
+     * <p>
      * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
-     *
+     * </p>
+     * 
      * @param p
      * @param q
      * @return
@@ -103,4 +145,243 @@ public class Solution {
         }
         return false;
     }
+
+    /**
+     * 637. Average of Levels in Binary Tree
+     * <p>
+     * https://leetcode.com/problems/average-of-levels-in-binary-tree
+     *
+     * <p>
+     * Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+     *
+     * Example 1: Input: 3 / \ 9 20 / \ 15 7 Output: [3, 14.5, 11] Explanation: The average value of nodes on level 0 is
+     * 3, on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11]. Note: The range of node's value is in
+     * the range of 32-bit signed integer.
+     * </p>
+     *
+     * @param root
+     * @return
+     */
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> treeNodeQueue = new LinkedList<>();
+
+        treeNodeQueue.add(root);
+        while (!treeNodeQueue.isEmpty()) {
+            double sum = 0.0;
+            int size = treeNodeQueue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = treeNodeQueue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    treeNodeQueue.offer(node.left);
+                }
+                if (node.right != null) {
+                    treeNodeQueue.offer(node.right);
+                }
+            }
+            result.add(sum / size);
+        }
+        return result;
+    }
+
+    /**
+     * 107. Binary Tree Level Order Traversal II
+     * 
+     * DFS Solution
+     * 
+     * <p>
+     * https://leetcode.com/problems/binary-tree-level-order-traversal-ii
+     * <p>
+     * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right,
+     * level by level from leaf to root).
+     * 
+     * For example: Given binary tree [3,9,20,null,null,15,7], 3 / \ 9 20 / \ 15 7
+     * 
+     * return its bottom-up level order traversal as: [ [15,7], [9,20], [3] ]
+     * </p>
+     * 
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrderBottomDFS(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(0, list);
+        }
+        return result;
+    }
+
+    /**
+     * BFS Solution
+     * 
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrderBottomBFS(TreeNode root) {
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        levelMaker(wrapList, root, 0);
+        return wrapList;
+    }
+
+    public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
+        if (root == null)
+            return;
+        if (level >= list.size()) {
+            list.add(0, new LinkedList<Integer>());
+        }
+        levelMaker(list, root.left, level + 1);
+        levelMaker(list, root.right, level + 1);
+        list.get(list.size() - level - 1).add(root.val);
+    }
+
+    /**
+     * 102. Binary Tree Level Order Traversal
+     * <p>
+     * https://leetcode.com/problems/binary-tree-level-order-traversal
+     * <p>
+     * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by
+     * level).
+     * 
+     * For example: Given binary tree [3,9,20,null,null,15,7], 3 / \ 9 20 / \ 15 7 return its level order traversal as:
+     * [ [3], [9,20], [15,7] ]
+     * </p>
+     * 
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
+    /**
+     * 111. Minimum Depth of Binary Tree
+     * <p>
+     * https://leetcode.com/problems/minimum-depth-of-binary-tree
+     * <p>
+     * Given a binary tree, find its minimum depth.
+     * 
+     * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf
+     * node.
+     * 
+     * 
+     * </p>
+     * 
+     * @param root
+     * @return
+     */
+    public int minDepthDFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return level;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return level;
+    }
+
+    public int minDepthRecursive(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepthRecursive(root.left);
+        int right = minDepthRecursive(root.right);
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+    }
+
+    /**
+     * 222. Count Complete Tree Nodes
+     * 
+     * <p>
+     * https://leetcode.com/problems/count-complete-tree-nodes
+     * <p>
+     * Given a complete binary tree, count the number of nodes.
+     * 
+     * Definition of a complete binary tree from Wikipedia:
+     * https://en.wikipedia.org/wiki/Binary_tree#Types_of_binary_trees
+     * 
+     * In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last
+     * level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+     * </p>
+     * 
+     * @param root
+     * @return
+     */
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int num = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                num++;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return num;
+    }
+
 }
