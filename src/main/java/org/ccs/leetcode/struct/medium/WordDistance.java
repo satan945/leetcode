559524394f34acf611940a41de5783da19bf362f
liqiang.fang <fangliq1986@gmail.com>
2017-09-07 18:50:51 -0700

    arrays

---
+++ src/main/java/org/ccs/leetcode/struct/medium/WordDistance.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 Liqiang Fang. All Rights Reserved.
+ */
+package org.ccs.leetcode.struct.medium;
+
+/**
+ * @author Abel created on 2017/9/7 12:42
+ * @version $Id$
+ */
+
+/**
+ * <p>
+ * This is a follow up of Shortest Word Distance.
+ * 
+ * https://leetcode.com/problems/shortest-word-distance
+ * 
+ * The only difference is now you are given the list of words and your method will be called repeatedly many times with
+ * different parameters. How would you optimize it?
+ * 
+ * Design a class which receives a list of words in the constructor, and implements a method that takes two words word1
+ * and word2 and return the shortest distance between these two words in the list.
+ * 
+ * For example, Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+ * 
+ * Given word1 = “coding”, word2 = “practice”, return 3. Given word1 = "makes", word2 = "coding", return 1.
+ * 
+ * Note: You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+ * 
+ * 
+ * </p>
+ */
+public class WordDistance {
+    public WordDistance(String[] words) {
+
+    }
+
+    public int shortest(String word1, String word2) {
+        return 0;
+    }
+}
